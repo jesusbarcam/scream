61f0d0c754af2ae8e452476da2ce7af7d65e4683
JesusBarcam <jesusbarcam@MacBook-Pro-de-JesusBarcam.local>
2022-01-29 17:56:29 +0100

    Installed react-native and init project

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Esto es el inicio de algo muy bonito!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
