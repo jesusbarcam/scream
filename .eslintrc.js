d3476eda627d8b0932decbaa0194aa2b2462a173
jesusBarcam <jesusbarcam@gmail.com>
2022-04-13 07:32:59 +0200

    Change no-unused-vars eslint rule to off

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,8 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
+        '@typescript-eslint/no-unused-vars': ['off'],
         'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
--- src/components/MultimediaKeyboard.tsx
+++ src/components/MultimediaKeyboard.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {View, StyleSheet, Linking} from 'react-native';
 
 import TouchableIcon from './TouchableIcon';
+import {useTheme} from '../hooks/UseTheme';
 import {
   DEFAULT_TWITTER_URL,
   DEFAULT_FACEBOOK_URL,
   DEFAULT_INSTAGRAM_URL,
 } from '../utils/ApplicationSettings';
-import {useTheme} from '../hooks/UseTheme';
 
 /**
  * @Component
@@ -15,7 +15,6 @@ import {useTheme} from '../hooks/UseTheme';
  * @description
  */
 export default function MultimediaKeyboard() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [theme, toggleTheme] = useTheme();
 
   return (
